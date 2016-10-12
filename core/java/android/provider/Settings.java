35b1c516eb09d1c40a224fcac91cbe824838955a
Simao Gomes Viana <xdevs23@outlook.com>
2016-10-12 16:43:08 +0200

    Settings: add TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3559,6 +3559,14 @@ public final class Settings {
                 }
             }
         };
+        
+        /**
+         * Whether touchscreen gestures give a haptic feedback
+         * 
+         * @hide
+         */
+        public static final String TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK =
+                                "touchscreen_gesture_haptic_feedback";
 
         /**
          * Beginning of Discovery System Settings Additions
@@ -3902,6 +3910,7 @@ public final class Settings {
             PUBLIC_SETTINGS.add(HAPTIC_FEEDBACK_ENABLED);
             PUBLIC_SETTINGS.add(SHOW_WEB_SUGGESTIONS);
             PUBLIC_SETTINGS.add(VIBRATE_WHEN_RINGING);
+            PUBLIC_SETTINGS.add(TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK);
         }
 
         /**
